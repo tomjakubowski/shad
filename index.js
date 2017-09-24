6e952bbba58e64de54a377a03f0d9571bd4fb460
Tom Jakubowski <tom@crystae.net>
2017-09-23 22:44:51 -0700

    dalling part 02
    note: ask for help about weird "halo" on triangle edges

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import fregl from 'regl';
-import {clamp} from './util';
+import baboon from 'baboon-image';
 
 // set up full window canvas + context
 const regl = fregl();
@@ -26,17 +26,47 @@ regl.clear({
   depth: 1.0
 });
 
-const triangle = regl({
+const checker = regl.texture({
+  data: [255, 0, 255, 255,  0, 255, 0, 255,
+         0, 255, 255, 255,  0, 0, 255, 255],
+  width: 2,
+  height: 2,
+  mag: 'linear',
+});
+
+const HazardImg = new Image();
+HazardImg.src = '/hazard.png';
+HazardImg.onload = () => {
+  hazard = regl.texture({
+    data: HazardImg,
+    min: 'linear',
+    mag: 'linear',
+    flipY: true
+  });
+};
+let hazard = checker;
+const baboonTex = regl.texture({data: baboon, flipY: true});
+
+const drawTriangle = regl({
   frag: `
 precision mediump float;
+
+uniform sampler2D tex;
+varying vec2 fr_TexCoord;
+
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  gl_FragColor = texture2D(tex, fr_TexCoord);
 }
 `,
   vert: `
 precision mediump float;
 attribute vec2 position;
+attribute vec2 texCoord;
+
+varying vec2 fr_TexCoord;
+
 void main() {
+  fr_TexCoord = texCoord;
   gl_Position = vec4(position, 0.0, 1.0);
 }
 `,
@@ -44,6 +74,19 @@ void main() {
     position: [[ 0.0,  0.8],
                [-0.8, -0.8],
                [ 0.8, -0.8]],
+    texCoord: [[ 0.5,  1.0],
+               [ 0.0,  0.0],
+               [ 1.0,  0.0]]
+  },
+  uniforms: {
+    tex: () => hazard
   },
   count: 3
-})();
+});
+
+regl.frame(({time}) => {
+  regl.clear({
+    color: [0, 0, 0, 1]
+  });
+  drawTriangle();
+});
